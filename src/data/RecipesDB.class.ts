718ee5c23719c4e14f263446fd3e19ee739ac9ed
Hugo Lacerda <hugolacerdar@gmail.com>
2020-07-17 14:55:06 -0300

    Entrega obrigatória

--- src/data/RecipesDB.class.ts
+++ src/data/RecipesDB.class.ts
@@ -1,23 +1,33 @@
 import knex from "knex";
 import BaseDB from "./BaseDB.class";
+import UserDB from "./UserDB.class";
 
 export interface Recipe {
-  id: string,
-  title: string,
-  description: string,
-  creator_id: string
+  id: string;
+  title: string;
+  description: string;
+  creator_id: string;
 }
 
 export default class RecipesDB extends BaseDB {
-
   private static TABLE_NAME = "CknRecipe";
 
-  public createRecipe = async(recipe: Recipe): Promise<void> =>{
+  public createRecipe = async (recipe: Recipe): Promise<void> => {
     await this.getConnection().raw(
       `
       INSERT INTO ${RecipesDB.TABLE_NAME}(id, title, description, creator_id, date)
       VALUES ("${recipe.id}", "${recipe.title}", "${recipe.description}", "${recipe.creator_id}", CURDATE());
       `
     );
-  }
+  };
+
+  public getById = async (id: string): Promise<any> => {
+    const result = await this.getConnection().raw(`
+     SELECT *
+     FROM ${RecipesDB.TABLE_NAME}
+      WHERE id = "${id}";
+    `);
+
+    return result[0][0];
+  };
 }
--- src/data/UserDB.class.ts
+++ src/data/UserDB.class.ts
@@ -10,6 +10,7 @@ export interface User {
 
 export default class UserDB extends BaseDB {
   private static TABLE_NAME = "CknUser";
+  private static RELATION_TABLE = "CknUsersRelation";
 
   public createUser = async (user: User): Promise<void> => {
     await this.getConnection().raw(
@@ -31,16 +32,66 @@ export default class UserDB extends BaseDB {
       `
     );
 
+    await BaseDB.destroyConnection();
+
     return result[0][0];
   };
 
   public getById = async (id: string): Promise<any> => {
     const result = await this.getConnection().raw(`
      SELECT *
-     FROM CknUser
+     FROM ${UserDB.TABLE_NAME}
       WHERE id = "${id}";
     `);
 
+    await BaseDB.destroyConnection();
+
     return result[0][0];
+  };
+
+  public follow = async (id: string, id_toFollow: string): Promise<void> => {
+    await this.getConnection().raw(
+      `
+      INSERT INTO ${UserDB.RELATION_TABLE}(user_id, follows_id)
+      VALUES ("${id}", "${id_toFollow}");
+      `
+    );
+
+    await BaseDB.destroyConnection();
+  };
+
+  public unfollow = async (
+    id: string,
+    id_toUnfollow: string
+  ): Promise<void> => {
+    await this.getConnection().raw(
+      `
+      DELETE FROM ${UserDB.RELATION_TABLE}
+      WHERE user_id = "${id}" 
+      AND follows_id = "${id_toUnfollow}";
+      `
+    );
+
+    await BaseDB.destroyConnection();
+  };
+
+  public getFeed = async (id: string): Promise<any> => {
+    const result = await this.getConnection().raw(
+      `
+      SELECT r.id, r.title, r.description, r.date AS createdAt, r.creator_id AS userId, c.name AS userName 
+      FROM CknUsersRelation ur  
+      INNER JOIN CknRecipe r
+      ON r.creator_id = ur.follows_id
+      INNER JOIN CknUser u
+      ON ur.user_id = u.id
+      INNER JOIN CknUser c
+      ON c.id = r.creator_id
+      WHERE u.id = "${id}";
+      `
+    );
+
+    await BaseDB.destroyConnection();
+
+    return result[0];
   };
 }
